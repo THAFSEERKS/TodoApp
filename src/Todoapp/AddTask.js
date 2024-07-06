bfa55fb7c48edf32e9c9d7186359555c79f15948
THAFSEERKS <muhammedthafseer.mec@gmail.com>
2024-07-06 12:46:59 -0400

    first commit

---
+++ src/Todoapp/AddTask.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AddTask = () => {
+  return (
+    <>
+        <div className='input-container'>
+            <input className="Input"  placeholder='Add a new Task' type='text'></input>
+            <button> ADD</button>
+        </div>
+    </>
+  )
+}
+
+export default AddTask
---
+++ src/Todoapp/Todo.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Todo.css'
+import AddTask from './AddTask';
+import ListTask from './ListTask';
+
+
+
+const Todo = () => {
+  return (
+    <>
+        <div className='todo-container'>
+            <div className='header'>TODO APP</div>
+            <div className='add-task'><AddTask/></div>
+            <div className='tasks'><ListTask/></div>
+        </div>      
+    </>
+  )
+}
+
+export default Todo;
